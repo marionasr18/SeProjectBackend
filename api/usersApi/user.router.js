df422b457f407652f2654d8bbfa916fd0a1bb3d7
OmarChaiban <omarchaiban21@gmail.com>
2023-04-24 21:57:24 +0300

    api

--- api/usersApi/user.router.js
+++ api/usersApi/user.router.js
@@ -1,10 +1,11 @@
-const {createUser,getUsers,getUserById,login,deleteUserById,updateUserById,updateUserProfileById}= require('./user.controller');
+const {createUser,getUsers,getUserById,login,deleteUserById,updateUserById,updateUserProfileById,getUserByName}= require('./user.controller');
 const router = require('express').Router();
 const {checkToken} =require('../../auth/token_validation') 
 
 router.post('/createUser',createUser)
 router.get('/getAllUser',checkToken,getUsers)
 router.get('/:id',checkToken,getUserById)
+router.get('/getUser/:name',checkToken,getUserByName)   
 router.post('/login',login)
 router.delete('/deleteUser/:id', deleteUserById);
 router.put('/updateUser/:id', checkToken,updateUserById);
